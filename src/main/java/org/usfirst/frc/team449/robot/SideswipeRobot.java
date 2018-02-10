39a5b76cce7c49bda5d5a2f571fb6d8e212e3a7d
Noah Gleason <noah.gleason1@gmail.com>
2018-02-10 16:29:52 -0500

    Add map for sideswipe only

---
+++ src/main/java/org/usfirst/frc/team449/robot/SideswipeRobot.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team449.robot;
+
+public class SideswipeRobot extends Robot{
+
+    /**
+     * The name of the map to read from. Should be overriden by a subclass to change the name.
+     */
+    protected String mapName = "sideswipe_map.yml";
+}
