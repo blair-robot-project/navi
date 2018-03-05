f3a4e8e88088378cee41a46cf92c154ee058ec3d
Noah Gleason <noah.gleason1@gmail.com>
2018-03-05 12:37:04 -0500

    Complete same-side auto

--- R scripts/drawMP.R
+++ R scripts/drawMP.R
@@ -198,11 +198,13 @@ centerToSide <- (34.5/2.)/12.
 #                                    rightInverted = c(FALSE, TRUE, FALSE, TRUE), 
 #                                    wheelbaseDiameter = wheelbaseDiameter, centerToBack = centerToBack, 
 #                                    startY = 11.092-centerToSide, robotFile = "navi.csv", intakeFile = "naviIntake.csv")
-totalOut <- executeProfileSequence(names = c("SameScale", "TurnToSwitch", "SameScaleToCube2","CubeToSwitchModified", "CubeToAlign","AlignToCube"), 
-                                   leftInverted = c(FALSE, FALSE, FALSE, FALSE, TRUE,FALSE),
-                                   rightInverted = c(FALSE, TRUE, FALSE, FALSE, TRUE,FALSE), 
-                                   wheelbaseDiameter = wheelbaseDiameter, centerToBack = centerToBack, 
+totalOut <- executeProfileSequence(names = c("SameScale", "TurnToSwitch", "SameScaleToCube2",
+                                             "CubeToSwitchModified", "CubeToAlign","AlignToCube",
+                                             "BackupToScale", "TurnToScale"), 
+                                   leftInverted = c(FALSE, FALSE, FALSE, FALSE, TRUE,FALSE, TRUE, TRUE),
+                                   rightInverted = c(FALSE, TRUE, FALSE, FALSE, TRUE,FALSE, TRUE, FALSE),
+                                   wheelbaseDiameter = wheelbaseDiameter, centerToBack = centerToBack,
                                    startY = 11.092-centerToSide, robotFile = "navi.csv", intakeFile = "naviIntake.csv")
 
-tracedAnimation(x=(totalOut[,2]+totalOut[,4])/2, y=(totalOut[,3]+totalOut[,5])/2, leftX = totalOut[,2], leftY = totalOut[,3], rightX = totalOut[,4], rightY = totalOut[,5],
-                headingRadians = totalOut[,6],deltaTime = 0.05,fieldFile = "powerUpField.csv",robotFile = "navi.csv", robotRadius = 2, filename="leftScale.mp4", robotCircleFile="naviIntake.csv")
+#tracedAnimation(x=(totalOut[,2]+totalOut[,4])/2, y=(totalOut[,3]+totalOut[,5])/2, leftX = totalOut[,2], leftY = totalOut[,3], rightX = totalOut[,4], rightY = totalOut[,5],
+#                headingRadians = totalOut[,6],deltaTime = 0.05,fieldFile = "powerUpField.csv",robotFile = "navi.csv", robotRadius = 2, filename="leftScale.mp4", robotCircleFile="naviIntake.csv")
